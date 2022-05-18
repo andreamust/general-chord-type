5e40c9fe3e99d6498d3de6c432a4f59de8fb469a
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-05-18 20:24:36 +0200

    updated basic pathfinder

--- src/general_chord_type.py
+++ src/general_chord_type.py
@@ -1,6 +1,7 @@
 """
 Re-implementation of the General Chord Type in an Object-Oriented fashion.
 """
+import sys
 from collections import namedtuple
 
 from utils import get_minimum
@@ -26,19 +27,46 @@ def get_minimum_weight(chord_grades: list, consonance_vector: list):
     :param consonance_vector:
     :return:
     """
-    minimum = 999
+
+    fixed_grades = [0, 3, 5, 9]
+
+    minimum = sys.maxsize
+
     paths = []
+    level = []
+    # while len(paths[0]) == len(chord_grades):
 
     for grade in chord_grades:
         combination = namedtuple('combination', 'root grade min')
-        weights = calculate_weights(grade, chord_grades, consonance_vector)
+        # get the total weight by iterating over all the chain
+        total_weight = []
+        for chain_el in grade:
+            weights = calculate_weights(chain_el, [x for x in fixed_grades if x not in grade], consonance_vector)
+            total_weight.append(weights)
+        weights = [sum(j) for j in zip(*total_weight)]
         print(grade, chord_grades, weights)
         combination_minimum, min_indexes = get_minimum(weights)
+        print('combination_minimum', combination_minimum, min_indexes)
         if combination_minimum <= minimum:
+            if combination_minimum < minimum:
+                level.clear()
             minimum = combination_minimum
-            paths.extend([combination(grade, chord_grades[x], minimum) for x in min_indexes])
+            reference_grades = [x[-1] for x in chord_grades if x[-1] not in grade]
+            print('rg', reference_grades)
+            paths.extend(grade)
+
+            level.extend([combination(grade[-1], reference_grades[x], minimum) for x in min_indexes])
+            print(level)
+    paths.extend([(combination.root, combination.grade) for combination in level])
+
+
     print(paths)
 
 
+def get_weight(chord_grades: list, consonance_vector: list):
+    for grade in chord_grades:
+        pass
+
+
 if __name__ == "__main__":
-    get_minimum_weight([0, 3, 5, 9], CV)
+    get_minimum_weight([[5, 0], [0, 3], [5, 9], [9, 0]], CV)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@
 
 import numpy as np
 
+from collections import namedtuple
+
 
 def get_minimum(values_list: list) -> tuple:
     """
@@ -14,3 +16,16 @@ def get_minimum(values_list: list) -> tuple:
     minimum = min(x for x in values_list if x > 0)
     values_array = np.array(values_list)
     return minimum, list(np.where(values_array == minimum)[0])
+
+
+def reformat_combinations(combinations_list: list[namedtuple], new_minimum: int) -> list:
+    """
+
+    :param new_minimum:
+    :param combinations_list:
+    :return:
+    """
+    for el in combinations_list:
+        if el.min > new_minimum:
+            combinations_list.remove(el)
+    return combinations_list
