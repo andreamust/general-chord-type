67ca2ff618f840d1816ede83b1b85963d6e9e6e2
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-05-19 00:28:59 +0200

    alpha version of the script

--- src/general_chord_type.py
+++ src/general_chord_type.py
@@ -20,7 +20,7 @@ def calculate_weights(root: int, grades: list, consonance_vector: list):
     return [consonance_vector[(x - root if x >= root else 12 + x - root)] for x in grades]
 
 
-def get_minimum_weight(chord_grades: list, consonance_vector: list):
+def get_minimum_path(chord_grades: list, consonance_vector: list):
     """
 
     :param chord_grades:
@@ -28,45 +28,39 @@ def get_minimum_weight(chord_grades: list, consonance_vector: list):
     :return:
     """
 
-    fixed_grades = [0, 3, 5, 9]
+    paths = [[g] for g in chord_grades]
+    # loop until the output has the same shape as the input
+    while len(paths[0]) != len(chord_grades):
+        # initialise the minimum at the max possible value (theoretically)
+        minimum = sys.maxsize
+        for step in paths:
+            # combination = namedtuple('combination', 'root grade min')
+            # calculate the grades to which the min_indexes refer
+            reference_grades = [x for x in chord_grades if x not in step]
+            # get the total weight by iterating over all the chain
+            total_weight = []
+            for chain_el in step:
+                weights = calculate_weights(chain_el, reference_grades, consonance_vector)
+                total_weight.append(weights)
+            weights = [sum(j) for j in zip(*total_weight)]
+            print(step, chord_grades, weights)
+            combination_minimum, min_indexes = get_minimum(weights)
+            print('combination_minimum', combination_minimum, min_indexes)
+            if combination_minimum <= minimum:
+                # if the minimum is lower than the achieved clean the list
+                if combination_minimum < minimum:
+                    paths.clear()
+                    minimum = combination_minimum
+                print('rg', reference_grades)
+                # store the minimum value path
+                for idx in min_indexes:
+                    paths.append(step + [reference_grades[idx]])
 
-    minimum = sys.maxsize
-
-    paths = []
-    level = []
-    # while len(paths[0]) == len(chord_grades):
-
-    for grade in chord_grades:
-        combination = namedtuple('combination', 'root grade min')
-        # get the total weight by iterating over all the chain
-        total_weight = []
-        for chain_el in grade:
-            weights = calculate_weights(chain_el, [x for x in fixed_grades if x not in grade], consonance_vector)
-            total_weight.append(weights)
-        weights = [sum(j) for j in zip(*total_weight)]
-        print(grade, chord_grades, weights)
-        combination_minimum, min_indexes = get_minimum(weights)
-        print('combination_minimum', combination_minimum, min_indexes)
-        if combination_minimum <= minimum:
-            if combination_minimum < minimum:
-                level.clear()
-            minimum = combination_minimum
-            reference_grades = [x[-1] for x in chord_grades if x[-1] not in grade]
-            print('rg', reference_grades)
-            paths.extend(grade)
-
-            level.extend([combination(grade[-1], reference_grades[x], minimum) for x in min_indexes])
-            print(level)
-    paths.extend([(combination.root, combination.grade) for combination in level])
-
+                # level.extend([combination(grade[-1], reference_grades[x], minimum) for x in min_indexes])
+                # print(level)
 
     print(paths)
 
 
-def get_weight(chord_grades: list, consonance_vector: list):
-    for grade in chord_grades:
-        pass
-
-
 if __name__ == "__main__":
-    get_minimum_weight([[5, 0], [0, 3], [5, 9], [9, 0]], CV)
+    get_minimum_path([0, 3, 5, 9], CV)
