7f38e1e1350f3ebc1e13850f5dee2f5290d638a8
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-05-19 16:19:56 +0200

    added harte utils and converters

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,6 @@
 
 import numpy as np
 
-from collections import namedtuple
-
 
 def get_minimum(values_list: list) -> tuple:
     """
@@ -16,16 +14,3 @@ def get_minimum(values_list: list) -> tuple:
     minimum = min(x for x in values_list if x > 0)
     values_array = np.array(values_list)
     return minimum, list(np.where(values_array == minimum)[0])
-
-
-def reformat_combinations(combinations_list: list[namedtuple], new_minimum: int) -> list:
-    """
-
-    :param new_minimum:
-    :param combinations_list:
-    :return:
-    """
-    for el in combinations_list:
-        if el.min > new_minimum:
-            combinations_list.remove(el)
-    return combinations_list
